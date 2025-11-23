26e3ce4bf64df83665cdd35484593b0f6a178e74
mugomes <murilogomes5792@gmail.com>
2025-11-23 20:38:11 -0300

    Fix theme and add new widgets

--- miconvertimage.go
+++ miconvertimage.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Murilo Gomes Julio
+// Copyright (C) 2024-2025 Murilo Gomes Julio
 // SPDX-License-Identifier: GPL-2.0-only
 
 // Site: https://mugomes.github.io
@@ -7,6 +7,8 @@ package main
 
 import (
 	"fmt"
+	"image/color"
+	m "mugomes/miconvertimage/modules"
 	"net/url"
 	"path/filepath"
 
@@ -15,25 +17,47 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 
-	// "fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/mugomes/mgcolumnview"
-	"github.com/mugomes/mgdialogopenfile"
+	"github.com/mugomes/mgdialogbox"
 	"github.com/mugomes/mgnumericentry"
 )
 
 const VERSION_APP string = "2.0.0"
 
-type myTheme struct {
-	fyne.Theme
+type myDarkTheme struct{}
+
+func (m myDarkTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	// A lógica para forçar o modo escuro é retornar cores escuras.
+	// O Fyne usa estas constantes internamente:
+	switch name {
+	case theme.ColorNameBackground:
+		return color.RGBA{28, 28, 28, 255} // Fundo preto
+	case theme.ColorNameForeground:
+		return color.White // Texto branco
+	// Adicione outros casos conforme a necessidade (InputBackground, Primary, etc.)
+	default:
+		// Retorna o tema escuro padrão para as outras cores (se existirem)
+		// Aqui estamos apenas definindo as cores principais para garantir o Dark Mode
+		return theme.DefaultTheme().Color(name, theme.VariantDark)
+	}
 }
 
-func (m myTheme) Size(name fyne.ThemeSizeName) float32 {
-	if name == theme.SizeNameText {
-		return 16 // Set your desired font size here
+// 3. Implemente os outros métodos necessários da interface fyne.Theme (usando o tema padrão)
+func (m myDarkTheme) Font(s fyne.TextStyle) fyne.Resource {
+	return theme.DefaultTheme().Font(s)
+}
+
+func (m myDarkTheme) Icon(n fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(n)
+}
+
+func (m myDarkTheme) Size(n fyne.ThemeSizeName) float32 {
+	if n == theme.SizeNameText {
+		return 16
 	}
-	return m.Theme.Size(name)
+	return theme.DefaultTheme().Size(n)
 }
 
 type sDados struct {
@@ -42,44 +66,43 @@ type sDados struct {
 }
 
 func main() {
-	sIcon, err := fyne.LoadResourceFromPath("icon/miconvertimage.png")
-	if err != nil {
-		panic(err)
-	}
+	m.LoadTranslations()
+
+	sIcon := fyne.NewStaticResource("miconvertimage.png", resourceMiConvertImagePngData)
 	a := app.NewWithID("br.com.mugomes.miconvertimage")
 	a.SetIcon(sIcon)
 	w := a.NewWindow("MiConvertImage")
 	w.Resize(fyne.NewSize(800, 600))
 	w.CenterOnScreen()
 	w.SetFixedSize(true)
-	a.Settings().SetTheme(&myTheme{theme.DarkTheme()})
+	a.Settings().SetTheme(&myDarkTheme{})
 
-	mnuAbout := fyne.NewMenu("Sobre",
-		fyne.NewMenuItem("Check Update", func() {
+	mnuAbout := fyne.NewMenu(m.T("About"),
+		fyne.NewMenuItem(m.T("Check Update"), func() {
 			url, _ := url.Parse("https://github.com/mugomes/miconvertimage/releases")
 			a.OpenURL(url)
 		}),
 		fyne.NewMenuItemSeparator(),
-		fyne.NewMenuItem("Support MiConvertImage", func() {
+		fyne.NewMenuItem(m.T("Support MiConvertImage"), func() {
 			url, _ := url.Parse("https://mugomes.github.io/apoie.html")
 			a.OpenURL(url)
 		}),
 		fyne.NewMenuItemSeparator(),
-		fyne.NewMenuItem("About MiConvertImage", func() {
+		fyne.NewMenuItem(m.T("About MiConvertImage"), func() {
 			showAbout(a)
 		}),
 	)
 
 	w.SetMainMenu(fyne.NewMainMenu(mnuAbout))
 
-	headers := []string{"Arquivo"}
+	headers := []string{m.T("File")}
 	sWidths := []float32{30, w.Canvas().Size().Width - 7}
 	data := [][]string{}
 
 	cv := mgcolumnview.NewColumnView(headers, sWidths, true)
 
-	btnAddFile := widget.NewButton("Add Imagem", func() {
-		fs := mgdialogopenfile.New(a, "Abrir Arquivo", []string{".webp", ".jpg", ".png"}, true, func(filenames []string) {
+	btnAddFile := widget.NewButton(m.T("Add Image"), func() {
+		mgdialogbox.NewOpenFile(a, m.T("Open File"), []string{".webp", ".jpg", ".png"}, true, func(filenames []string) {
 			for _, filename := range filenames {
 				ext := filepath.Ext(filename)
 				if ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".webp" {
@@ -91,23 +114,22 @@ func main() {
 						filename,
 					})
 				} else {
-					dialog.ShowInformation("MiCheckHash", "Formato Inválido! Somente arquivos PNG, JPG ou WEBP são aceitos.", w)
+					dialog.ShowInformation("MiCheckHash", m.T("Invalid format! Only PNG, JPG, or WEBP files are accepted."), w)
 				}
 
 			}
 		})
-		fs.Show()
 	})
 	btnAddFile.Resize(fyne.NewSize(137, 30))
 	btnAddFile.Move(fyne.NewPos(7, 7))
 
-	btnRemoveFile := widget.NewButton("Remover Selecionados", func() {
+	btnRemoveFile := widget.NewButton(m.T("Remove Selected"), func() {
 		cv.RemoveSelected()
 	})
 	btnRemoveFile.Resize(fyne.NewSize(197, 30))
 	btnRemoveFile.Move(fyne.NewPos(btnAddFile.Size().Width+19, 7))
 
-	btnRemoveFiles := widget.NewButton("Remover Todos", func() {
+	btnRemoveFiles := widget.NewButton(m.T("Remove All"), func() {
 		cv.RemoveAll()
 	})
 	btnRemoveFiles.Resize(fyne.NewSize(159, 30))
@@ -116,7 +138,7 @@ func main() {
 	cv.Resize(fyne.NewSize(w.Canvas().Size().Width-7, 300))
 	cv.Move(fyne.NewPos(0, btnAddFile.Position().Y+37))
 
-	lblFormat := widget.NewLabel("Formatos")
+	lblFormat := widget.NewLabel(m.T("Formats"))
 	lblFormat.TextStyle = fyne.TextStyle{Bold: true}
 	lblFormat.Resize(fyne.NewSize(137, 30))
 	lblFormat.Move(fyne.NewPos(0, cv.Position().Y+300))
@@ -125,34 +147,41 @@ func main() {
 	cboFormat.Resize(fyne.NewSize(137, 38))
 	cboFormat.Move(fyne.NewPos(7, lblFormat.Position().Y+37))
 
-	lblQualidade := widget.NewLabel("Qualidade")
+	lblQualidade := widget.NewLabel(m.T("Quality"))
 	lblQualidade.TextStyle = fyne.TextStyle{Bold: true}
-	lblQualidade.Move(fyne.NewPos(cboFormat.Size().Width+17, cv.Position().Y+300))
+	lblQualidade.Move(fyne.NewPos(cboFormat.Size().Width+12, cv.Position().Y+300))
 	txtQualidade := mgnumericentry.NewMGNumericEntryWithButtons(0, 100, 90)
 	txtQualidade.Resize(fyne.NewSize(100, 38))
 	txtQualidade.Move(fyne.NewPos(cboFormat.Size().Width+17, lblQualidade.Position().Y+37))
-	
-	// lblTamanho := widget.NewLabel("Tamanho")
-	// lblTamanho.TextStyle = fyne.TextStyle{Bold: true}
-	// lblTamanho.Resize(fyne.NewSize(100, 30))
-	// lblTamanho.Move(fyne.NewPos(cboFormat.Size().Width+17, cv.Position().Y+300))
-	// txtTamanhoWidth := widget.NewEntry()
-	// txtTamanhoWidth.Resize(fyne.NewSize(50, 38))
-	// txtTamanhoWidth.Move(fyne.NewPos(cboFormat.Size().Width+24, lblTamanho.Position().Y+37))
-	// lblX := widget.NewLabel("x")
-	// lblX.Resize(fyne.NewSize(50, 38))
-	// lblX.Move(fyne.NewPos(txtTamanhoWidth.Position().X+49, lblTamanho.Position().Y+37))
-	// txtTamanhoHeight := widget.NewEntry()
-	// txtTamanhoHeight.Resize(fyne.NewSize(50,38))
-	// txtTamanhoHeight.Move(fyne.NewPos(lblX.Position().X+24, lblTamanho.Position().Y+37))
 
-	btnConvert := widget.NewButton("Gerar", func() {
+	lblTamanho := widget.NewLabel(m.T("Size"))
+	lblTamanho.TextStyle = fyne.TextStyle{Bold: true}
+	lblTamanho.Resize(fyne.NewSize(100, 30))
+	lblTamanho.Move(fyne.NewPos(cboFormat.Size().Width+82, cv.Position().Y+300))
+	txtTamanhoWidth := widget.NewEntry()
+	txtTamanhoWidth.Resize(fyne.NewSize(50, 38))
+	txtTamanhoWidth.Move(fyne.NewPos(cboFormat.Size().Width+89, lblTamanho.Position().Y+37))
+	lblX := widget.NewLabel("x")
+	lblX.Resize(fyne.NewSize(50, 38))
+	lblX.Move(fyne.NewPos(txtTamanhoWidth.Position().X+49, lblTamanho.Position().Y+37))
+	txtTamanhoHeight := widget.NewEntry()
+	txtTamanhoHeight.Resize(fyne.NewSize(50,38))
+	txtTamanhoHeight.Move(fyne.NewPos(lblX.Position().X+24, lblTamanho.Position().Y+37))
+
+	lblProporcao := widget.NewLabel("Proporção")
+	lblProporcao.Move(fyne.NewPos(txtTamanhoHeight.Position().X+52, cv.Position().Y+300))
+	sProporcao := []string{"Manter", "Não Manter"}
+	cboProporcao := widget.NewSelectEntry(sProporcao)
+	cboProporcao.SetText("Manter")
+	cboProporcao.Resize(fyne.NewSize(137, 38))
+	cboProporcao.Move(fyne.NewPos(txtTamanhoHeight.Position().X+59, lblProporcao.Position().Y+37))
+
+	btnConvert := widget.NewButton(m.T("Generate"), func() {
 		s := &sDados{}
 		s.imagens = cv.ListAll()
 		s.format = cboFormat.Text
 		s.showConvert(a)
 	})
-
 
 	btnConvert.Resize(fyne.NewSize(157, 59))
 	btnConvert.Move(fyne.NewPos(7, cboFormat.Position().Y+67))
@@ -165,10 +194,12 @@ func main() {
 		cboFormat,
 		lblQualidade,
 		txtQualidade,
-		// lblTamanho,
-		// txtTamanhoWidth,
-		// lblX,
-		// txtTamanhoHeight,
+		lblTamanho,
+		txtTamanhoWidth,
+		lblX,
+		txtTamanhoHeight,
+		lblProporcao,
+		cboProporcao,
 		btnConvert,
 	)
 	w.SetContent(layout)
