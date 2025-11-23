39d22af022e5ea68afe8f4edfbdb7e2a3dc50407
mugomes <murilogomes5792@gmail.com>
2025-11-23 20:40:03 -0300

    Replaced json to map

--- modules/translate.go
+++ modules/translate.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Murilo Gomes Julio
+// Copyright (C) 2024-2025 Murilo Gomes Julio
 // SPDX-License-Identifier: GPL-2.0-only
 
 // Site: https://mugomes.github.io
@@ -6,7 +6,6 @@
 package modules
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -18,40 +17,41 @@ var tr Translations
 
 // Detecta o idioma do sistema e retorna apenas o código base (pt, en, es, etc.)
 func GetSystemLanguage() string {
-    lang := os.Getenv("LANG")
-    if lang == "" {
-        lang = os.Getenv("LC_ALL")
-    }
-    if lang == "" {
-        lang = os.Getenv("LC_MESSAGES")
-    }
-    if lang == "" {
-        return "en" // fallback padrão
-    }
+	lang := os.Getenv("LANG")
+	if lang == "" {
+		lang = os.Getenv("LC_ALL")
+	}
+	if lang == "" {
+		lang = os.Getenv("LC_MESSAGES")
+	}
+	if lang == "" {
+		return "en" // fallback padrão
+	}
 
-    // Exemplo: "pt_BR.UTF-8" → "pt"
-    parts := strings.Split(lang, ".")
-    base := parts[0]
-    baseParts := strings.Split(base, "_")
-    return strings.ToLower(baseParts[0])
+	// Exemplo: "pt_BR.UTF-8" → "pt"
+	parts := strings.Split(lang, ".")
+	base := parts[0]
+	baseParts := strings.Split(base, "_")
+	return strings.ToLower(baseParts[0])
 }
 
-func LoadTranslations() error {
-    file, err := os.Open("langs/" + GetSystemLanguage() + ".json")
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    decoder := json.NewDecoder(file)
-    return decoder.Decode(&tr)
+func LoadTranslations() {
+	if GetSystemLanguage() == "pt" {
+		valor := make(map[string]string)
+		valor["About"] = "Sobre"
+		valor["Check Update"] = "Verificar Atualização"
+		valor["Support MiConvertImage"] = "Apoie MiConvertImage"
+		valor["About MiConvertImage"] = "Sobre MiConvertImage"
+		valor["Add Image"] = "Add Imagem"
+		tr = valor
+	}
 }
 
 // T retorna o texto traduzido com formatação opcional.
 func T(key string, args ...interface{}) string {
-    msg, ok := tr[key]
-    if !ok {
-        msg = key // fallback se não achar
-    }
-    return fmt.Sprintf(msg, args...)
+	msg, ok := tr[key]
+	if !ok {
+		msg = key // fallback se não achar
+	}
+	return fmt.Sprintf(msg, args...)
 }
