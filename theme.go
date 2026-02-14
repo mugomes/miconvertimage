4c4254099264ba2f74e49aebd696637b1ec493c9
mugomes <murilogomes5792@gmail.com>
2026-02-14 13:03:32 -0300

    Comments removed.

--- theme.go
+++ theme.go
@@ -10,22 +10,16 @@ import (
 type myDarkTheme struct{}
 
 func (m myDarkTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
-	// A lógica para forçar o modo escuro é retornar cores escuras.
-	// O Fyne usa estas constantes internamente:
 	switch name {
 	case theme.ColorNameBackground:
 		return color.RGBA{28, 28, 28, 255} // Fundo preto
 	case theme.ColorNameForeground:
-		return color.White // Texto branco
-	// Adicione outros casos conforme a necessidade (InputBackground, Primary, etc.)
+		return color.White
 	default:
-		// Retorna o tema escuro padrão para as outras cores (se existirem)
-		// Aqui estamos apenas definindo as cores principais para garantir o Dark Mode
 		return theme.DefaultTheme().Color(name, theme.VariantDark)
 	}
 }
 
-// 3. Implemente os outros métodos necessários da interface fyne.Theme (usando o tema padrão)
 func (m myDarkTheme) Font(s fyne.TextStyle) fyne.Resource {
 	return theme.DefaultTheme().Font(s)
 }
